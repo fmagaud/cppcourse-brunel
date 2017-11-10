929c52e2964d02c110eed9155c2190caea17f596
fmagaud <32614246+fmagaud@users.noreply.github.com>
2017-11-10 15:40:35 +0100

    Delete CTestTestfile.cmake

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/INTRANET/fmagaud/myfiles/cpp/semestre3/neurons_project
-# Build directory: /home/INTRANET/fmagaud/myfiles/cpp/semestre3/neurons_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(unit_test "unit_test")
-subdirs(gtest)
